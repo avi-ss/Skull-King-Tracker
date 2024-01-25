f6c7ff75d870e26293679bd65167e21874fafbea
Alberto Longo <alber7longo@gmail.com>
2024-01-25 17:17:26 +0100

    Added deploy config to gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Skull-King-Tracker',
+  base: '/Skull-King-Tracker/',
   build: { outDir: './build' }
 })
