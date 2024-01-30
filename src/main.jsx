60ddd6d88dd28a88beaf7e443c110d03c5fcfdc0
Alberto Longo <alber7longo@gmail.com>
2024-01-30 11:21:27 +0100

    Config project for multiple deploys

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,17 @@ import './index.css'
 
 import { GameProvider } from './context/GameContext'
 import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+const basename = import.meta.env.VITE_BASE_URL || '/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
     <GameProvider>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path='/' element={<App />}></Route>
+          <Route path='*' element={<Navigate to="/" />}></Route>
         </Routes>
       </BrowserRouter>
     </GameProvider>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  build: { outDir: './build' }
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    base: env.VITE_BASE_URL,
+    build: { outDir: './build' }
+  });
+}
